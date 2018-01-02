b842015a1e3b1a12223d758a3f59f48b5ab7b3ff
petetanru <pete.tanru@gmail.com>
2018-01-02 17:12:24 +0700

    stanford + kaggle weight, model taken out of loop, timer added

--- model.py
+++ model.py
@@ -15,14 +15,25 @@ def get_features(MODEL, data):
     features = cnn_model.predict(data, batch_size=256, verbose=1)
     return features
 
-def linear(features, out_sz):
-    inputs = Input(features.shape[1:])
+def pretrain(MODEL):
+    h, w = 299, 299
+    cnn_model = MODEL(include_top=False, input_shape=(h, w, 3), weights='imagenet')
+    inputs = Input((h, w, 3))
+    x = inputs
+    x = Lambda(preprocess_input, name='preprocessing')(x)
+    x = cnn_model(x)
+    x = GlobalAveragePooling2D()(x)
+    cnn_model = Model(inputs, x)
+    return cnn_model
+
+def linear(inp_sz, out_sz):
+    inputs = Input(inp_sz)
     x = inputs
     # x = BatchNormalization()(x)
     # x = Dense(1024)(x)
     # x = BatchNormalization()(x)
-    x = Dense(256)(x)
-    x = Activation('relu')(x)
+    # x = Dense(256)(x)
+    # x = Activation('relu')(x)
     x = Dropout(0.5)(x)
     x = Dense(out_sz, activation='softmax')(x)
     model = Model(inputs, x)
--- predict.py
+++ predict.py
@@ -1,3 +1,6 @@
+import time
+start = time.time()
+
 import os
 import numpy as np
 import pandas as pd
@@ -5,25 +8,38 @@ from glob import glob
 import cv2
 from utils import path_to_tensor
 from keras.applications import *
-from model import get_features, linear
+from model import get_features, linear, pretrain
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+
+
+inception_sz = (1536,)
 X = np.zeros((299, 299, 3), dtype=np.float32)
 sorted_breed = np.load('breed_sorted.npy')
 
+pretrain_model = pretrain(InceptionResNetV2)
+classifier = linear(inp_sz=inception_sz, out_sz=120)
+classifier.load_weights('weights.best.inceptresnetv2.comb.hdf5')
+
 pred_images = np.array(glob("pic_pred/*"))
+
 for dog_pic in pred_images:
+    time_loop = time.time()
     tensor = path_to_tensor(dog_pic)
-    inception_features = get_features(InceptionResNetV2, tensor)
-
-    # TODO: state this before the loop, change model file too
-    model = linear(inception_features, out_sz=120)
-    model.load_weights('weights.best.inceptresnetv2.hdf5')
-
-    y_pred = model.predict(inception_features, batch_size=1)
+    time_vectorize = time.time()
+    features = pretrain_model.predict(tensor, batch_size=256, verbose=1)
+    time_feature = time.time()
+    y_pred = classifier.predict(features, batch_size=1)
     pred = np.argmax(y_pred, axis=1)
+    time_classify = time.time()
     print(dog_pic, "is a ", sorted_breed[pred])
+    print("vectorize time", time_vectorize - time_loop)
+    print("forward pass time", time_feature - time_vectorize)
+    print("classify time", time_classify - time_feature)
+    print("total time per img", time_classify - time_loop)
 
 
 
+end = time.time()
+print (end - start)
